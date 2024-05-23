e60e60609d3dde03c5093365b2668b6994e15dbc
karl0905 <kalle44@live.dk>
2024-05-23 13:25:07 +0200

    tpope, vim-repeat and harpoon hotkeys

--- nvim/lua/karl/packer.lua
+++ nvim/lua/karl/packer.lua
@@ -7,6 +7,8 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  use 'tpope/vim-repeat'
+
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     -- or                            , branch = '0.1.x',
