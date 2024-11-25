a5a20378609838403adc718687ba2a9e5887d637
karl0905 <kalle44@live.dk>
2024-11-25 19:33:33 +0100

    made nvim transparent

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,4 +1,10 @@
 vim.cmd [[colorscheme tokyonight-night]]
+vim.cmd [[ 
+  highlight Normal guibg=none 
+  highlight NonText guibg=none 
+  highlight Normal guibg=none 
+  highlight NonText ctermbg=none 
+]]
 
 function HiLine()
   vim.cmd([[hi LineNr guibg=none guifg=#B7B7B7]])
