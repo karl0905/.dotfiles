117b69089d13a253a97098839641c290c5cce76a
karl0905 <kalle44@live.dk>
2024-11-25 20:04:57 +0100

    changed .wezterm.lua to original file, symlinked it to ~/

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,1 +1,20 @@
-/Users/karl/.wezterm.lua
+-- Pull in the wezterm API
+local wezterm = require('wezterm');
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 14
+
+config.enable_tab_bar = false
+
+config.window_decorations = "TITLE | RESIZE"
+
+config.color_scheme = "Tokyo Night"
+
+config.window_background_opacity = 0.97
+config.macos_window_background_blur = 10
+
+return config
+
