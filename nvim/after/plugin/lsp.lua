8b4c70d781392f4ec5350ca4fa3b06f2fff4e69a
karl0905 <kalle44@live.dk>
2025-02-17 16:16:43 +0100

    style(lsp): format lol

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -29,7 +29,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.prettierd,     -- use prettierd for formatting
+    null_ls.builtins.formatting.prettierd, -- use prettierd for formatting
   },
 })
 
