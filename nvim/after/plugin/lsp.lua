51d4e2f388843b05699cee815184ed8dad049e39
karl0905 <kalle44@live.dk>
2025-02-28 09:37:45 +0100

    fix(lsp): optimize formatting and lsp attachments prevent null-ls from attaching to nerdtree buffer

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -29,11 +29,33 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.prettierd, -- use prettierd for formatting
+    null_ls.builtins.formatting.prettierd,                                                                -- use prettierd for formatting
   },
+  filetypes = { "lua", "css", "javascript", "typescript", "javascriptreact", "typescriptreact", "html" }, -- Apply to these filetypes
 })
 
-require 'lspconfig'.emmet_ls.setup {
+local function on_attach(client, bufnr)
+  if vim.bo[bufnr].filetype == "nerdtree" then
+    client.detach(bufnr)
+    return
+  end
+
+  local opts = { buffer = bufnr, remap = false }
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end
+
+require('lspconfig').emmet_ls.setup {
+  on_attach = on_attach,
   capabilities = {
     textDocument = {
       completion = {
@@ -48,12 +70,7 @@ require 'lspconfig'.emmet_ls.setup {
 
 -- Setup vim-dadbod for SQL completion
 require('lspconfig').sqls.setup {
-  on_attach = function(client, bufnr)
-    -- Enable SQL completion
-    vim.cmd [[
-        autocmd FileType sql setlocal omnifunc=v:lua.vim.dadbod.complete
-    ]]
-  end,
+  on_attach = on_attach,
 }
 
 -- Fix Undefined global 'vim'
@@ -84,21 +101,6 @@ lsp.set_preferences({
     info = 'I'
   }
 })
-
-lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
-
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
 
 lsp.setup()
 
