1bbe3e53b9b8c562e4ade52b3d6e87b8ea83c8ae
karl0905 <kalle44@live.dk>
2025-02-28 10:58:54 +0100

    fix(lsp): fix issues with lsps not installing correctly

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,81 +1,55 @@
 local lsp = require("lsp-zero")
-
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-  'rust_analyzer', -- Rust Language Server
-  'ts_ls',         -- TypeScript Language Server
-})
+-- Setup Mason
+require('mason').setup()
 
-require('mason').setup({
+-- Setup mason-lspconfig
+require('mason-lspconfig').setup({
   ensure_installed = {
+    'rust_analyzer',
+    'ts_ls',
     'cssls',
-    'emmet_ls',
-    'eslint',
     'html',
     'intelephense',
     'lua_ls',
-    'prettierd',
-    'rust_analyzer',
     'tailwindcss',
-    'ts_ls',
     'sqls',
     'jedi_language_server',
     'pylsp',
   },
 })
 
-local null_ls = require("null-ls")
-
-null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.prettierd,                                                                -- use prettierd for formatting
+-- Setup mason-null-ls
+require('mason-null-ls').setup({
+  ensure_installed = {
+    'prettierd',
+    'eslint_d',
   },
-  filetypes = { "lua", "css", "javascript", "typescript", "javascriptreact", "typescriptreact", "html" }, -- Apply to these filetypes
+  automatic_installation = true,
 })
 
-local function on_attach(client, bufnr)
-  if vim.bo[bufnr].filetype == "nerdtree" then
-    client.detach(bufnr)
-    return
-  end
-
-  local opts = { buffer = bufnr, remap = false }
-
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end
-
-require('lspconfig').emmet_ls.setup {
-  on_attach = on_attach,
-  capabilities = {
-    textDocument = {
-      completion = {
-        completionItem = {
-          snippetSupport = true,
-        },
-      },
-    },
+-- Setup null-ls
+local null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.prettierd,
   },
-  filetypes = { "html", "css", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-}
+})
 
 -- Setup vim-dadbod for SQL completion
 require('lspconfig').sqls.setup {
-  on_attach = on_attach,
+  on_attach = function(client, bufnr)
+    -- Enable SQL completion
+    vim.cmd [[
+        autocmd FileType sql setlocal omnifunc=v:lua.vim.dadbod.complete
+    ]]
+  end,
 }
 
--- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
+-- Setup cmp
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -84,7 +58,6 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
-
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
@@ -101,6 +74,20 @@ lsp.set_preferences({
     info = 'I'
   }
 })
+
+lsp.on_attach(function(client, bufnr)
+  local opts = { buffer = bufnr, remap = false }
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
 
 lsp.setup()
 
