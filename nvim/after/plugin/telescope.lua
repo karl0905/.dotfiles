e3ebbcd17d69c30f0d4ae636fdb972150cdec355
karl0905 <kalle44@live.dk>
2025-05-08 19:47:56 +0200

    feat(telescope): add live grep and search buffers

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,6 +2,5 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 -- git files search på Ctrl + p, dette søger efter alle git filer
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader>ps', builtin.live_grep)
+vim.keymap.set('n', '<leader>pb', builtin.buffers)
