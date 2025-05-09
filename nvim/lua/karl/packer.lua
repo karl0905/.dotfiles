be06c39ebbaaab5d91fcc558d74a134053ad6671
karl0905 <kalle44@live.dk>
2025-05-09 16:46:52 +0200

    style(nvim_packer): format nvim packer file

--- nvim/lua/karl/packer.lua
+++ nvim/lua/karl/packer.lua
@@ -1,160 +1,159 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  use 'tpope/vim-repeat'
+	use("tpope/vim-repeat")
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    -- or                            , branch = '0.1.x',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-  use 'folke/tokyonight.nvim'
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-      ts_update()
-    end, }
-  use("nvim-treesitter/playground")
-  use("theprimeagen/harpoon")
-  use("mbbill/undotree")
-  use('tpope/vim-fugitive')
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("folke/tokyonight.nvim")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
+	use("nvim-treesitter/playground")
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
 
-  use { 'neovim/nvim-lspconfig' }
-  use { 'williamboman/mason.nvim' }
-  use { 'williamboman/mason-lspconfig.nvim' }
-  use { 'stevearc/conform.nvim' }
-  use { 'mfussenegger/nvim-lint' }
+	use({ "neovim/nvim-lspconfig" })
+	use({ "williamboman/mason.nvim" })
+	use({ "williamboman/mason-lspconfig.nvim" })
+	use({ "stevearc/conform.nvim" })
+	use({ "mfussenegger/nvim-lint" })
 
-  -- Autocompletion
-  use { 'hrsh7th/nvim-cmp' }
-  use { 'hrsh7th/cmp-buffer' }
-  use { 'hrsh7th/cmp-path' }
-  use { 'saadparwaiz1/cmp_luasnip' }
-  use { 'hrsh7th/cmp-nvim-lsp' }
-  use { 'hrsh7th/cmp-nvim-lua' }
+	-- Autocompletion and snippets
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
+	use("onsails/lspkind.nvim")
 
-  -- Snippets
-  use { 'L3MON4D3/LuaSnip' }
-  use { 'rafamadriz/friendly-snippets' }
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
+	use({
+		"kylechui/nvim-surround",
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	})
+	use("jbyuki/instant.nvim")
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
+	use("github/copilot.vim")
+	use("christoomey/vim-tmux-navigator")
+	use("preservim/nerdtree")
+	use({
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  use({
-    "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end
-  })
-  use('jbyuki/instant.nvim')
-  use {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-  }
-  use 'github/copilot.vim'
-  use 'christoomey/vim-tmux-navigator'
-  use 'preservim/nerdtree'
-  use {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require("nvim-autopairs").setup {}
-    end
-  }
+	-- vi-mongo.nvim
+	use({
+		"kopecmaciej/vi-mongo.nvim",
+		config = function()
+			require("vi-mongo").setup()
+		end,
+	})
 
-  -- vi-mongo.nvim
-  use {
-    'kopecmaciej/vi-mongo.nvim',
-    config = function()
-      require('vi-mongo').setup()
-    end
-  }
+	-- none-ls
+	use("nvimtools/none-ls.nvim")
 
-  -- none-ls
-  use 'nvimtools/none-ls.nvim'
+	-- vim-windowswap
+	use("wesQ3/vim-windowswap")
 
-  -- vim-windowswap
-  use 'wesQ3/vim-windowswap'
-
-  -- neotest
-  use {
-    "nvim-neotest/neotest",
-    requires = {
-      "olimorris/neotest-rspec",
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter"
-    },
-    config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-rspec"),
-        }
-      })
-    end
-  }
-  -- dadbod for SQL
-  use 'tpope/vim-dadbod'
-  use 'kristijanhusak/vim-dadbod-ui'
-  use 'kristijanhusak/vim-dadbod-completion'
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    },
-    config = function()
-      require('gitsigns').setup {
-        signs                        = {
-          add          = { text = '│' },
-          change       = { text = '│' },
-          delete       = { text = '_' },
-          topdelete    = { text = '‾' },
-          changedelete = { text = '~' },
-          untracked    = { text = '┆' },
-        },
-        signcolumn                   = true,
-        numhl                        = false,
-        linehl                       = false,
-        word_diff                    = false,
-        watch_gitdir                 = {
-          follow_files = true
-        },
-        attach_to_untracked          = true,
-        current_line_blame           = false,
-        current_line_blame_opts      = {
-          virt_text = true,
-          virt_text_pos = 'eol',
-          delay = 1000,
-          ignore_whitespace = false,
-          virt_text_priority = 100,
-        },
-        current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-        sign_priority                = 6,
-        update_debounce              = 100,
-        status_formatter             = nil,
-        max_file_length              = 40000,
-        preview_config               = {
-          border = 'single',
-          style = 'minimal',
-          relative = 'cursor',
-          row = 0,
-          col = 1
-        },
-      }
-    end
-  }
+	-- neotest
+	use({
+		"nvim-neotest/neotest",
+		requires = {
+			"olimorris/neotest-rspec",
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-rspec"),
+				},
+			})
+		end,
+	})
+	-- dadbod for SQL
+	use("tpope/vim-dadbod")
+	use("kristijanhusak/vim-dadbod-ui")
+	use("kristijanhusak/vim-dadbod-completion")
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "│" },
+					change = { text = "│" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				signcolumn = true,
+				numhl = false,
+				linehl = false,
+				word_diff = false,
+				watch_gitdir = {
+					follow_files = true,
+				},
+				attach_to_untracked = true,
+				current_line_blame = false,
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol",
+					delay = 1000,
+					ignore_whitespace = false,
+					virt_text_priority = 100,
+				},
+				current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+				sign_priority = 6,
+				update_debounce = 100,
+				status_formatter = nil,
+				max_file_length = 40000,
+				preview_config = {
+					border = "single",
+					style = "minimal",
+					relative = "cursor",
+					row = 0,
+					col = 1,
+				},
+			})
+		end,
+	})
 end)
