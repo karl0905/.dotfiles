5f28a64ab4c8d0b5969eaa794464640902632b28
karl0905 <kalle44@live.dk>
2025-05-30 13:34:02 +0200

    fix(nvim_telescope): add --fixed-strings to grep

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,12 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 -- git files search på Ctrl + p, dette søger efter alle git filer
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep)
-vim.keymap.set('n', '<leader>pb', builtin.buffers)
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", function()
+	builtin.live_grep({
+		additional_args = function()
+			return { "--fixed-strings" }
+		end,
+	})
+end)
+vim.keymap.set("n", "<leader>pb", builtin.buffers)
