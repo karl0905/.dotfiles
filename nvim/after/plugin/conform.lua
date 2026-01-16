9d9a266e7727e0878c063f64fcb006a4b8c125b0
karl0905 <kalle44@live.dk>
2026-01-16 15:13:26 +0100

    style(nvim): formatting

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -1,41 +1,41 @@
 local conform = require("conform")
 conform.setup({
-	formatters_by_ft = {
-		javascript = { "eslint_d" },
-		typescript = { "eslint_d" },
-		javascriptreact = { "eslint_d" },
-		typescriptreact = { "eslint_d" },
-		svelte = { "eslint_d" },
-		css = { "eslint_d" },
-		html = { "eslint_d" },
-		json = { "prettierd" },
-		yaml = { "prettierd" },
-		markdown = { "markdownlint" },
-		graphql = { "eslint_d" },
-		lua = { "stylua" },
-		python = { "isort", "ruff" },
-		sh = { "shfmt" },
-		bash = { "shfmt" },
+  formatters_by_ft = {
+    javascript = { "eslint_d" },
+    typescript = { "eslint_d" },
+    javascriptreact = { "eslint_d" },
+    typescriptreact = { "eslint_d" },
+    svelte = { "eslint_d" },
+    css = { "eslint_d" },
+    html = { "eslint_d" },
+    json = { "prettierd" },
+    yaml = { "prettierd" },
+    markdown = { "markdownlint" },
+    graphql = { "eslint_d" },
+    lua = { "stylua" },
+    python = { "isort", "ruff" },
+    sh = { "shfmt" },
+    bash = { "shfmt" },
     typst = { "typstyle" },
-	},
-	formatters = {
-		csharpier = {
-			command = "csharpier",
-			args = { "format", "$FILENAME" },
-			stdin = false,
-		},
-	},
-	-- format_on_save = {
-	-- 	lsp_fallback = true,
-	-- 	async = false,
-	-- 	timeout_ms = 1000,
-	-- },
+  },
+  formatters = {
+    csharpier = {
+      command = "csharpier",
+      args = { "format", "$FILENAME" },
+      stdin = false,
+    },
+  },
+  -- format_on_save = {
+  -- 	lsp_fallback = true,
+  -- 	async = false,
+  -- 	timeout_ms = 1000,
+  -- },
 })
 
 vim.keymap.set({ "n", "v" }, "<leader>f", function()
-	conform.format({
-		lsp_fallback = true,
-		async = false,
-		timeout_ms = 1000,
-	})
+  conform.format({
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 1000,
+  })
 end, { desc = "Format file or range (in visual mode)" })
