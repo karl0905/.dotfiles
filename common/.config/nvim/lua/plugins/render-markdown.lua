2c0bc7b39b9dc7240c6c38c16c2b33f59eb3f522
karl0905 <kalle44@live.dk>
2026-04-29 10:20:48 +0200

    fix(nvim): start render-markdown disabled

--- common/.config/nvim/lua/plugins/render-markdown.lua
+++ common/.config/nvim/lua/plugins/render-markdown.lua
@@ -4,6 +4,7 @@ require("render-markdown").setup({
   -- requires = { "echasnovski/mini.nvim", opt = true }, -- if you use the mini.nvim suite
   -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
   requires = { "nvim-tree/nvim-web-devicons", opt = true }, -- if you prefer nvim-web-devicons
+  enabled = false -- start disabled
 })
 
 -- Render markdown toggle
